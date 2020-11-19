a485be96f3f3b6f216b954e136a85cfeab9e7836
JohnnyKaster <44278869+Johnnykaster@users.noreply.github.com>
2020-11-18 17:48:02 -0800

    Still working on rendering

--- scripts/render_questions.js
+++ scripts/render_questions.js
@@ -34,6 +34,12 @@ let questionValues = [100, 200, 300, 400, 500];
  *      HINT: Use the + operator on a string and a variable to join them!
  *      Need more help? Check out https://bit.ly/2EIsD3b or ask a volunteer.
  */
+
 function renderQuestions() {
-    // Add code here
+    for(let i = 0; i < numCategories; i++) {
+        questionValues.forEach(val => {
+            $("#questions").append(`<div class="question"><div class="question-title">${val}</div></div>`);
+            $("#questions").append(`<div class='question-cell' data-category="${i}">${i}</div>`);
+        })
+    }
 }
